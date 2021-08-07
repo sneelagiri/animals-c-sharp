96e00c6c2d5aa9721deef6dd3e897bf49d8dc646
Shashank Neelagiri <sneelagiri@outlook.com>
2021-08-07 14:19:42 +0200

    Name and images for observable collection items

--- animals-c-sharp/Pages/Index.cshtml.cs
+++ animals-c-sharp/Pages/Index.cshtml.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -10,11 +9,9 @@ namespace animals_c_sharp.Pages
 {
     public class IndexModel : PageModel
     {
+
         public void OnGet()
         {
-
         }
-
-        ObservableCollection<Animal> AnimalsCollection = new ObservableCollection<Animal> { };
     }
 }
